cdc2d8e6541abfce2005fedb132aeea36e740838
MariiaAmetina <ametina.ms@gmail.com>
2020-08-26 07:33:38 +0300

    arrow function

--- script.js
+++ script.js
@@ -5259,4 +5259,12 @@ console.log(users); */
 
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
-//Функция every и some
+//Стрелочные функции в JavaScript
+// let result = every([1, 2, 3, 4, 5], elem => elem > 0);
+
+// let result1 = every([1, 2, 3, 4, 5], (elem, index) => elem * index > 10);
+
+// let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// let result2 = each(arr, (elem, index) => elem * index > 10);
+
+
