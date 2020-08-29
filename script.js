8b62dfd8c511047a2ada2a4ec1723d1f8a917c42
MariiaAmetina <ametina.ms@gmail.com>
2020-08-29 17:53:20 +0300

    forEach method

--- script.js
+++ script.js
@@ -5605,12 +5605,47 @@ console.log(users); */
 //////////////////////////////////////////////////
 /////////////////////////////////////////////////
 //Перебор многомерных массивов
-let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
-let result = arr.map(function(elem) {
-	return elem.map(function(num) {
-		return num * num;
-	});
-});
-console.log(result);
+// let result = arr.map(function(elem) {
+// 	return elem.map(function(num) {
+// 		return num * num;
+// 	});
+// });
+// console.log(result);
 
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////
+/////////////////////////////////////////////////
+//Метод forEach для перебора массива в JavaScript
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// let result = arr.forEach(function(elem, index) {
+// 	document.write(index + '. ' + elem + '<br>');
+// });
+
+
+//Дан массив с числами. Используя метод forEach найдите сумму квадратов элементов этого массива.
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// arr.forEach(function(elem) {
+// 	sum += elem ** 2;
+// });
+// console.log(sum);
+
+
+
+
+
+
+
+//////////////////////////////////////////////////
+/////////////////////////////////////////////////
+//Метод filter для фильтрации массива в JavaScript
