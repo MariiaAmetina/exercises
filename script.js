595a48953a3ed747028548187e3c2a73a494540d
MariiaAmetina <ametina.ms@gmail.com>
2020-08-30 20:26:43 +0300

    Метод some

--- script.js
+++ script.js
@@ -5712,8 +5712,23 @@ console.log(users); */
 // let result = arr.every(elem => elem > 0);
 // console.log(result);
 
+// let arr = [2, 4, 6, 8];
+// let result = arr.every((elem, index) => elem * index < 30);
+// console.log(result);
 
 
-let arr = [2, 4, 6, 8];
-let result = arr.every((elem, index) => elem * index < 30);
+
+
+
+//////////////////////////////////////////////////
+////////////////////////////////////////////////////
+//Метод some для проверки массива в JavaScript
+
+// let arr = [-2, -4, 6, -8];
+// let result = arr.some(elem => elem > 0);
+// console.log(result);
+
+let arr = [2, 4, 6, 8, 10];
+let result = arr.some((elem, index) => elem * index < 30);
 console.log(result);
+
