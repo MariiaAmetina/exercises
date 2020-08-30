4838830f0910f51e921803fb85ea2207f1a6ec69
MariiaAmetina <ametina.ms@gmail.com>
2020-08-30 21:02:14 +0300

    Operator SPREAD...

--- script.js
+++ script.js
@@ -5757,3 +5757,30 @@ console.log(users); */
 // let arr = [1, 2, 3, 4, 5];
 // let min = Math.min(...arr);
 // console.log(min);
+
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+
+// let arr  = ['a', ...arr1, 'b', 'c', ...arr2];
+// console.log(arr);
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+
+// let arr  = ['a', ...arr1, ...arr1, 'b', 'c'];
+// console.log(arr);
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [...arr1, 4, 5, 6];
+
+// let arr  = ['a', 'b', 'c', ...arr2];
+// console.log(arr);
+
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [...arr1, 4, 5, 6];
+// let arr3 = [...arr2, 7, 8, 9];
+
+// let arr  = [0, ...arr3];
+// console.log(arr);
