cdb4b44733f87aa36f706c1a1ac7ed286d7366bd
MariiaAmetina <ametina.ms@gmail.com>
2020-08-30 20:21:37 +0300

    Метод every

--- script.js
+++ script.js
@@ -5708,3 +5708,12 @@ console.log(users); */
 //////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 //Метод every для проверки массива в JavaScript
+// let arr = [-2, 4, 6, 8];
+// let result = arr.every(elem => elem > 0);
+// console.log(result);
+
+
+
+let arr = [2, 4, 6, 8];
+let result = arr.every((elem, index) => elem * index < 30);
+console.log(result);
