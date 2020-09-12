c8ca6eea8b69bf401f91cc34e9d831803c2d74b7
MariiaAmetina <ametina.ms@gmail.com>
2020-09-12 08:39:23 +0300

    Получение DOM эдемента в JS

--- script.js
+++ script.js
@@ -6176,3 +6176,37 @@ console.log(users); */
 // 	console.log(width);
 // 	console.log(height);
 // }
+
+let button = document.querySelector('#button');
+console.log(button);
+
+let elem1 = document.querySelector('#elem1');
+console.log(elem1);
+
+let elem2 = document.querySelector('#elem2');
+console.log(elem2);
+
+let elem3 = document.querySelector('#elem3');
+console.log(elem3);
+
+
+let elem = document.querySelector('#parent input');
+console.log(elem);
+
+
+let element = document.querySelector('.elem');
+console.log(element);
+
+
+let firstElem = document.querySelector('#block p');
+console.log(firstElem);
+
+
+
+let firstClass = document.querySelector('.block p');
+console.log(firstClass);
+
+
+
+let elemClass = document.querySelector('.www');
+console.log(elemClass);
